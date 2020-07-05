3e9c491e89b84a1c7872b68639ee610813c1e7b7
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-05 21:23:06 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,9 +22,9 @@ end
 
 def return_string_array(array)
   count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
+  whilecount < array.length do
+    new_array.push(array[count].to_s)
+    count += 1 
   end
-  array
+  new_array
 end
